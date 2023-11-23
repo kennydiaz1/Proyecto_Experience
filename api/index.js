b4e9d980560ea5aa1d2a97872dd2477fdbc8826d
Christian <christianbur1248@gmail.com>
2023-11-23 18:41:56 -0500

    Deploy API

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:5173', 'https://1962-190-147-196-36.ngrok-free.app/'],
+  origin: true,
 }));
 
 async function uploadToS3(path, originalFilename, mimetype) {
